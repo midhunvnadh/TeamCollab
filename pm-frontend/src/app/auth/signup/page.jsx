717864d3fcb7f6f02f1d69eb4d680c74a994c516
Midhun V Nadh <midhun.vnadh5732@gmail.com>
2025-04-10 01:04:14 +0530

    feat: implement signup functionality with form handling and redirect on success

--- pm-frontend/src/app/auth/signup/page.jsx
+++ pm-frontend/src/app/auth/signup/page.jsx
@@ -1,7 +1,30 @@
+"use client";
+import request from "@/lib/request";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 import React from "react";
 
 export default function signup() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [password_c, setPassword_c] = React.useState("");
+
+  const signUp = async (e) => {
+    e.preventDefault();
+
+    const { data } = await request.post("/auth/signup", {
+      username,
+      password,
+    });
+
+    if (data.success) {
+      alert("User created successfully");
+      redirect("/auth/login");
+    } else {
+      alert(data.message);
+    }
+  };
+
   return (
     <div className="h-svh w-full flex items-center justify-center">
       <div className="dark:bg-base-300 bg-gray-50 p-5 w-sm rounded-sm space-y-3">
@@ -13,18 +36,36 @@ export default function signup() {
             <h2>Sign Up</h2>
           </div>
         </div>
-        <form action="" className="space-y-4">
+        <form onSubmit={signUp} className="space-y-4">
           <label htmlFor="" className="input flex items-center w-full">
             <span>Username</span>
-            <input type="text" name="username" className="w-full" />
+            <input
+              type="text"
+              name="username"
+              className="w-full"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
           </label>
           <label htmlFor="" className="input flex items-center w-full">
             <span>Password</span>
-            <input type="password" name="password" className="w-full" />
+            <input
+              type="password"
+              name="password"
+              className="w-full"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </label>
           <label htmlFor="" className="input flex items-center w-full">
             <span>Confirm Password</span>
-            <input type="password" name="password_c" className="w-full" />
+            <input
+              type="password"
+              name="password_c"
+              className="w-full"
+              onChange={(e) => setPassword_c(e.target.value)}
+              value={password_c}
+            />
           </label>
           <div>
             <button className="btn btn-primary w-full">Sign Up</button>
