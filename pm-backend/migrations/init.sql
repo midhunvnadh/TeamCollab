b2eb5dea2eeb0a05a8bc791a9362e9be325ba835
Midhun V Nadh <midhun.vnadh5732@gmail.com>
2025-04-10 21:44:46 +0530

    feat: update database schema for users, projects, user_projects_access, and tasks tables

--- pm-backend/migrations/init.sql
+++ pm-backend/migrations/init.sql
@@ -1,28 +1,33 @@
 CREATE TABLE IF NOT EXISTS public.users (
-    id serial NOT NULL PRIMARY KEY,
-    username text NOT NULL,
+    id serial PRIMARY KEY,
+    username text NOT NULL UNIQUE,
     password text NOT NULL,
     created_at time with time zone NOT NULL DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS public.user_projects_access (
-    id serial NOT NULL PRIMARY KEY,
-    user_id integer NULL,
-    project_id integer NULL,
-    admin boolean NOT NULL DEFAULT false
+CREATE TABLE IF NOT EXISTS public.projects (
+    id serial PRIMARY KEY,
+    name text NOT NULL UNIQUE,
+    created_at timestamp with time zone NOT NULL DEFAULT now()
 );
-  
+
+CREATE TABLE IF NOT EXISTS public.user_projects_access (
+    id serial PRIMARY KEY,
+    user_id integer NOT NULL,
+    project_id integer NOT NULL,
+    admin boolean NOT NULL DEFAULT false,
+    FOREIGN KEY (user_id) REFERENCES public.users (id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES public.projects (id) ON DELETE CASCADE,
+    UNIQUE (user_id, project_id)
+);
+
 CREATE TABLE IF NOT EXISTS public.tasks (
-    id serial NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     project_id integer NOT NULL,
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     status integer NOT NULL DEFAULT 0,
     title text NOT NULL,
-    assigned_to_user integer NULL
-);
-
-CREATE TABLE IF NOT EXISTS public.projects (
-    id serial NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    created_at timestamp with time zone NOT NULL DEFAULT now()
+    assigned_to_user integer,
+    FOREIGN KEY (project_id) REFERENCES public.projects (id) ON DELETE CASCADE,
+    FOREIGN KEY (assigned_to_user) REFERENCES public.users (id) ON DELETE SET NULL
 );
