8192b6935f58618197707c5a9d47392efc927e22
Midhun V Nadh <midhun.vnadh5732@gmail.com>
2025-04-13 01:01:51 +0530

    feat: add responsive padding to project page container

--- pm-frontend/src/app/projects/page.jsx
+++ pm-frontend/src/app/projects/page.jsx
@@ -27,7 +27,7 @@ export default function page() {
     fetchProjects();
   }, []);
   return (
-    <div className="">
+    <div className="px-4 lg:p-0">
       <div className="container mx-auto sticky top-5 mt-5">
         <div className="w-full p-2 bg-base-300 flex items-center justify-between rounded-xl px-2">
           <div className="font-black tracking-wide">
